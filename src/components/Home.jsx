751e396a7f7f4cecf9b72ca9cd7359e63686c007
user1 <user01@em/com>
2025-05-26 06:35:46 +0300

    update home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -54,31 +54,33 @@ function Home() {
   }
 
   return (
-    <div className="p-3">
+    <div className="p-3 lg:flex flex-col justify-center items-center bg-gray-600">
+
+    <h1 className="lg:text-xl font-bold py-3 text-white">Staff Engineer - Payments Screening - Riyadh</h1>
 
 
-    <div className="flex flex-col items-start gap-3 bg-gray-200 rounded-xl p-3 h-full lg:h-auto w-full">
+    <div className="flex flex-col items-start gap-3 bg-gray-200 rounded-xl p-3 lg:w-6/10">
       <div className="flex gap-2">
-        <label className="p-1 w-20 font-bold">username</label>
-        <input value={username} onChange={(e) => setUsername(e.target.value)} className="border rounded p-1 w-60 lg:w-100 bg-white focus:outline-indigo-600" type="text" />
+        <label className="p-1 w-30 font-bold">Full Name</label>
+        <input value={username} onChange={(e) => setUsername(e.target.value)} className=" rounded p-1 w-60 lg:w-100 bg-white focus:outline-indigo-600" type="text" />
       </div>
       <div className="flex gap-2">
-        <label className="p-1 w-20 font-bold">age</label>
-        <input value={date} onChange={(e) => setDate(e.target.value)} className="border rounded p-1 w-60 lg:w-100 bg-white focus:outline-indigo-600" type="date" />
+        <label className="p-1 w-30 font-bold">Date of birth</label>
+        <input value={date} onChange={(e) => setDate(e.target.value)} className=" rounded p-1 w-60 lg:w-100 bg-white focus:outline-indigo-600" type="date" />
       </div>
       <div className="flex gap-2">
-        <label className="p-1 w-20 font-bold">City</label>
-          <select value={city} onChange={(e) => setCity(e.target.value)} className="border rounded w-60 lg:w-100 bg-white focus:outline-indigo-600">
-            <option selected>Choose a country</option>
-            <option value="United States">United States</option>
-            <option value="Canada">Canada</option>
-            <option value="France">France</option>
-            <option value="Germany">Germany</option>
+        <label className="p-1 w-30 font-bold">City</label>
+          <select value={city} onChange={(e) => setCity(e.target.value)} className=" rounded w-60 lg:w-100 bg-white focus:outline-indigo-600">
+            <option selected>Choose a City</option>
+            <option value="Qassim">Qassim</option>
+            <option value="Riyadh">Riyadh</option>
+            <option value="Jiddah">Jiddah</option>
+            <option value="Mecca">Mecca</option>
           </select>
       </div>
       <div className="flex gap-2">
-        <label className="p-1 w-20 font-bold">Salary</label>
-          <select value={salary} onChange={(e) => setSalary(e.target.value)} className="border rounded w-60 lg:w-100 bg-white focus:outline-indigo-600">
+        <label className="p-1 w-30 font-bold">Salary</label>
+          <select value={salary} onChange={(e) => setSalary(e.target.value)} className=" rounded w-60 lg:w-100 bg-white focus:outline-indigo-600">
             <option selected>Choose a Salary</option>
             <option value="salary 1">between 3000 to 8000</option>
             <option value="salary 2">between 9000 to 13000</option>
@@ -87,42 +89,42 @@ function Home() {
       </div>
       <div className="flex flex-col gap-2">
         <label className="p-1 font-bold">why did you apply for this jop?</label>
-        <textarea value={description} onChange={(e) => setDescription(e.target.value)} className="border rounded w-60 lg:w-122 bg-white focus:outline-indigo-600"></textarea>
+        <textarea value={description} onChange={(e) => setDescription(e.target.value)} className=" rounded w-60 lg:w-122 bg-white focus:outline-indigo-600"></textarea>
       </div>
       <div className="flex flex-col gap-2">
         <label className="p-1 font-bold">Are you registered in GOSI?</label>
         <div className="flex items-center">
-          <input value={"yes"} onChange={(e) => setGOSIregestered(e.target.value)} type="radio"  name="GOSI-radio" className="h-5 w-5" />
+          <input value={"yes"} onChange={(e) => setGOSIregestered(e.target.value)} type="radio"  name="GOSI-radio" className="h-5 w-5 accent-black" />
           <label className="px-3 ">Yes</label>
         </div>
         <div className="flex items-center">
-          <input value={"no"} onChange={(e) => setGOSIregestered(e.target.value)} type="radio"  name="GOSI-radio" className="h-5 w-5" />
+          <input value={"no"} onChange={(e) => setGOSIregestered(e.target.value)} type="radio"  name="GOSI-radio" className="h-5 w-5 accent-black" />
           <label className="px-3">No</label>
         </div>
       </div>
       <div className="flex flex-col gap-2">
         <label className="p-1 font-bold">Do you claim any disability?</label>
         <div className="flex items-center">
-          <input value={"yes"} onChange={(e) => setCheckDisability(e.target.value)} type="radio"  name="default-radio" className="h-5 w-5" />
+          <input value={"yes"} onChange={(e) => setCheckDisability(e.target.value)} type="radio"  name="default-radio" className="h-5 w-5 accent-black" />
           <label className="px-3">Yes</label>
         </div>
         <div className="flex items-center">
-          <input value={"no"} onChange={(e) => setCheckDisability(e.target.value)} type="radio"  name="default-radio" className="h-5 w-5" />
+          <input value={"no"} onChange={(e) => setCheckDisability(e.target.value)} type="radio"  name="default-radio" className="h-5 w-5 accent-black" />
           <label className="px-3">No</label>
         </div>
       </div>
       <div className="flex flex-col gap-2">
         <label className="p-1 font-bold">Do you have more than 2 years of experience in AI?</label>
         <div className="flex items-center">
-          <input value={"yes"} onChange={(e) => setExp(e.target.value)} type="radio"  name="AI-radio" className="h-5 w-5" />
+          <input value={"yes"} onChange={(e) => setExp(e.target.value)} type="radio"  name="AI-radio" className="h-5 w-5 accent-black" />
           <label className="px-3">Yes</label>
         </div>
         <div className="flex items-center">
-          <input value={"no"} onChange={(e) => setExp(e.target.value)} type="radio"  name="AI-radio" className="h-5 w-5" />
+          <input value={"no"} onChange={(e) => setExp(e.target.value)} type="radio"  name="AI-radio" className="h-5 w-5 accent-black" />
           <label className="px-3">No</label>
         </div>
       </div>
-      <button onClick={checkData} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Submit</button>
+      <button onClick={checkData} className="bg-violet-500 hover:bg-violet-700 text-white font-bold py-2 px-4 rounded">Apply Now</button>
     </div>
 
     </div>
